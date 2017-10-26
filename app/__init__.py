fed3a6a55d8650073df5ef8ce4883b7c5465f30d
LittleBai0606 <958255724@qq.com>
2017-10-26 23:12:45 +0800

    完善数据库和实现用户注册登录登出功能

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 app = Flask(__name__)
 app.config.from_object('config')
+#初始化数据库
 db = SQLAlchemy(app)
+
+#初始化flask-login
+lm = LoginManager()
+lm.init_app(app)
 
 from app import views, models
 
--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, PasswordField
-from wtforms.validators import Required, DataRequired
+from wtforms import StringField, BooleanField, PasswordField, SubmitField
+from wtforms.validators import Required, DataRequired,Email,Length
 
 class LoginForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember_me', default=False)
+    user_name = StringField('user name', validators=[DataRequired(), Length(max=15)])
+    remember_me = BooleanField('remember me', default=False)
+    submit = SubmitField('Log in')
+
+class SignUpForm(FlaskForm):
+    user_name = StringField('user name', validators=[DataRequired(), Length(max=15)])
+    user_email = StringField('user email', validators=[Email(), DataRequired(), Length(max=128)])
+    submit = SubmitField('Sign up')
